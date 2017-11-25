501b00b32aa798c94db3ca57db389d4af34257dd
samhanndean <chauhanprateek89@gmail.com>
2017-11-25 11:20:53 +0000

    csv imported

--- news-app.py
+++ news-app.py
@@ -5,14 +5,23 @@ Created on Sat Nov 25 10:28:21 2017
 @author: Prateek Chauhan
 """
 
+import csv
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
+def get_csv():
+    csv_path = './static/la-riots-deaths.csv'
+    csv_file = open(csv_path,'rb')
+    csv_obj = csv.reader(csv_file)
+    csv_list = list(csv_obj)
+    return csv_list
+
 @app.route("/")
 def index():
     template = 'index.html'
-    return render_template(template)
+    object_list = get_csv()
+    return render_template(template,object_list=object_list)
 
 if __name__ == '__main__':
     # Fire up the Flask test server
